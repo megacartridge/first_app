cdc4f4d67cdcd36bbe97366fe1465aa1377ca8d1
megacartridge <mdltrack@gmail.com>
2013-12-16 17:53:07 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f76351a1840c79713fba25412b515034bf5e19a561f27588c210e2e40c3d3ae60067da50e275559090df45fe762e10c92882af6ed77c7437f0aba092af048bbd'
